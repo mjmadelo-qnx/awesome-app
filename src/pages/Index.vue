018f3781c466bad925d4171a1965c7ef037f2281
mjmadelo-qnx <mj.madelo@questronix.com.ph>
2019-11-09 14:40:58 +0800

    update

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,21 @@
 <template>
   <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+    <!-- <img alt="Quasar logo" src="~assets/quasar-logo-full.svg"> -->
+    <template>
+      <div class="q-pa-md" style="max-width: 350px">
+        <q-list bordered separator>
+          
+
+          <q-item clickable v-ripple>
+            <q-item-section>
+              <q-item-label>Basang Basa sa Ulan</q-item-label>
+              <q-item-label caption>Aegis</q-item-label>
+            </q-item-section>
+          </q-item>
+
+        </q-list>
+      </div>
+    </template>
   </q-page>
 </template>
 
