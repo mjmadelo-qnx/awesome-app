270777fcde917625fcc29f291c3b04101a023c5d
mjmadelo-qnx <mj.madelo@questronix.com.ph>
2019-11-10 15:25:18 +0800

    modified form

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -10,10 +10,12 @@
         <!-- computed object declarative rendering -->
         {{ showSongAndArtist }}
 
-        <q-input outlined v-model="artist" label="Artist" />
-        <q-input outlined v-model="songTitle" label="Song Name" />
+        <!-- <q-input outlined v-model="artist" label="Artist" />
+        <q-input outlined v-model="songTitle" label="Song Name" /> -->
 
-        
+        <q-input outlined v-model="name" label="Full Name" />
+        <q-input outlined v-model="height" label="Height (in cm)" />
+        <q-input outlined v-model="weight" label="Weight (in kg)" />
 
         <!-- first way -->
         <!-- <q-btn v-if="!edit" color="secondary" class="full-width" label="Add Music" @click="addMusic()"/>
@@ -21,7 +23,13 @@
 
         <!-- ternary operator syntax -->
         <!-- condition ? true result : false result -->
-        <q-btn :color="!edit ? 'secondary' : 'primary'" class="full-width" :label="!edit ? 'Add Music' : 'Update Music'" @click="!edit ? addMusic() : updateMusic()"/>
+        <!-- <q-btn :color="!edit ? 'secondary' : 'primary'" class="full-width" :label="!edit ? 'Add Music' : 'Update Music'" @click="!edit ? addMusic() : updateMusic()"/> -->
+      
+        
+        <q-btn color="primary" class="full-width" label="Calculate BMI" @click="calculateBmi()"/>
+        
+        
+        
         <q-list bordered separator>
 
           <q-item class="musicList" clickable v-ripple v-for="(music, index) in musicList" :key="index">
@@ -35,7 +43,19 @@
               <!-- delete -->
               <q-btn color="red" icon="delete" @click="deleteMusic(music.id)"/>
             </q-item-section>
+          </q-item>
 
+          <q-item class="musicList" clickable v-ripple v-for="(bmi, index) in bmiList" :key="index">
+            <q-item-section>
+              <q-item-label>{{ index+1 }}: {{ bmi.name }} - {{ bmi.result }}</q-item-label>
+              <q-item-label caption>Height: {{ music.height }}, Weight: {{ music.weight }}</q-item-label>
+            </q-item-section>
+            <q-item-section right>
+              <!-- edit -->
+              <q-btn color="primary" icon="edit" @click="editMusic(music, music.id)"/>
+              <!-- delete -->
+              <q-btn color="red" icon="delete" @click="deleteMusic(music.id)"/>
+            </q-item-section>
           </q-item>
 
           <!-- <q-item-section right>
@@ -81,6 +101,16 @@ export default {
       this.$db.collection("musicList").add(obj);
       this.cancelEdit();
       // this.$router.push('/login');
+    },
+    addBmi () {
+      // use this kapag nasa loob ng script
+      var obj = {
+        name: this.name,
+        height: this.height,
+        weight: this.weight
+      };
+      this.$db.collection("musicList").add(obj);
+      this.cancelEdit();
     },
     cancelEdit () {
       this.artist = "";
@@ -130,8 +160,13 @@ export default {
         // { songTitle: "Reflection", artist: "Leah Salonga" },
         // { songTitle: "Where is the Love?", artist: "Black Eyed Peas" }
       ],
+      bmiList: [],
       artist: "",
       songTitle: "",
+      name: "",
+      height: "",
+      weight: "",
+      calculatedBmi: "",
       index: null,
       edit: false
     }
